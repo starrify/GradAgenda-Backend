e471a5fd3fb9229086d1b4412b13f267d20d209d
Carlso Yang <yangji9181@163.com>
2014-07-02 00:54:17 +0800

    master without data model

--- code/backend/urls.py
+++ code/backend/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from backend import settings, views
-from backend.register.views import show, register
+#from backend.register.views import show, register
 
 from django.contrib import admin
 admin.autodiscover()
@@ -13,8 +13,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index),
-    url(r'^site_media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
-    url(r'^register/$', register),
-    url(r'^register/show/$', show),
+#    url(r'^site_media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT}),
+#    url(r'^register/$', register),
+#    url(r'^register/show/$', show),
 )
 
